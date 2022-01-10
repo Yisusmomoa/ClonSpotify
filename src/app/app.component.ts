a82f1dea2853124029fb2d53a2675b8402e7de34
Yisusmomoa <bjls29@hotmail.com>
2022-01-10 03:02:07 -0600

    primer commit, se agregan varios modulos, los de auth, home, tracks,history

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'spotify1-a';
+  // name:string='brandon';//la variable name es de tipo string
+  // age:number=24;
+  // phone:null=null;
+  // phone1:undefined=undefined;
+  // dato:any;//puede almacenar cualquier tipo de dato, objeto, función, etc
+  // status: string | number=0;//puede tener dos tipos de datos
+  // car:carModel={
+  //   brand:'ford',
+  //   model:'focus',
+  //   year:2021
+  // }
+
+  // // listCars:Array<any>=[1,"hoal",null]//pueden ir cualquier tipo de dato
+  // listCars:Array<carModel>=[
+  //   {
+  //   brand:"chevrolet",
+  //   model:"camaro",
+  //   year:1960
+  //   },
+  //   {
+  //     brand:"bmw",
+  //     model:"gtr3",
+  //     year:2005
+  //   }
+  // ]
+
+
+}
+// interface carModel{
+//   brand:string,
+//   model:string,
+//   year?:number //se indica que este dato es opcional  
+// }
